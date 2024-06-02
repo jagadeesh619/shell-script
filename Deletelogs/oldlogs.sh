d192e2a04a43676e102bd8f4ac4329d4db15e8f0
jaga319 <mummadijagadeesh219@gmail.com>
2024-06-02 08:17:29 +0530

    delete logs

---
+++ Deletelogs/oldlogs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+cd /tmp/roboshop-logs
+
+if [ $? -ne 0]
+then
+    echo "Directory is not available Please check "
+fi
+
+files=$(find /tmp/roboshop-logs -type f -mtime +14 -name "*.log")
+
+
+while IFS= read -r file
+do
+    echo "deleting the log file : $file"
+    rm -rf $file
+done <<<  $files
