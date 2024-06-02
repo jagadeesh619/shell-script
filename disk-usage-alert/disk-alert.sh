290872a2f8fb48cfab0aae7cbbc9e2db247a65c4
jaga319 <mummadijagadeesh219@gmail.com>
2024-06-02 10:23:51 +0530

    delete logs

---
+++ disk-usage-alert/disk-alert.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+disk_usage=$(df -hT | grep -vE 'tmp|File') 
+disk_limit=1
+message=""
+
+while IFS= read -r line
+do
+    disk_per=$( echo $line | awk '{print $6}' | cut -d % -f1)
+    disk_filesys=$(echo $line | awk 'print $1')
+    if [ $disk_per -gt $disk_limit ]
+    then
+        message+="High Disk usage on $disk_filesys : $disk_per \n"
+    fi
+done <<< $disk_usage
+
+echo -e "$message" | | mail -s "High Disk Usage" mummadijagadeesh143@gmail.com
+        
