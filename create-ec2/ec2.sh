efcdb32e85a507c27db9f184f114b40b67c8ba68
jaga319 <mummadijagadeesh219@gmail.com>
2024-06-02 17:28:35 +0530

    ec2 instance create

--- create-ec2/ec2.sh
+++ create-ec2/ec2.sh
@@ -7,14 +7,15 @@ instances=("mongodb" "catalogue" "web")
 
 for i in "${instances[@]}"
 do  
-    echo "instance : $i"
     if [ $i=="mongodb" ]
     then
         instances_type="t3.micro"
     else
         instances_type="t2.micro"
     fi
-    aws ec2 run-instances --image-id $ami_id --count 1 --instance-type $instances_type  --security-group-ids $sg_id     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
+    Private_ip=$(aws ec2 run-instances --image-id $ami_id --count 1 --instance-type $instances_type  --security-group-ids $sg_id     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text) 
+    echo "instance : $i : $Private_ip"
+
 done
 
 
