386ad0fc74badbd0193dfb7f751b6230834e11d8
jaga319 <mummadijagadeesh219@gmail.com>
2024-06-02 10:27:37 +0530

    disk space alert

--- disk-usage-alert/disk-alert.sh
+++ disk-usage-alert/disk-alert.sh
@@ -7,7 +7,7 @@ message=""
 while IFS= read -r line
 do
     disk_per=$( echo $line | awk '{print $6}' | cut -d % -f1)
-    disk_filesys=$(echo $line | awk 'print $1')
+    disk_filesys=$(echo $line | awk '{print $1}')
     if [ $disk_per -gt $disk_limit ]
     then
         message+="High Disk usage :  $disk_filesys : $disk_per \n"
